297e89fe46a15630043fada1b5ff4636961b32c7
Rukshan Dissanayake <rukshanmali@gmail.com>
2024-12-15 00:54:27 +0200

    Contact customer service test completed 100%

--- src/test/java/com/ui/pages/ContactUsPage.java
+++ src/test/java/com/ui/pages/ContactUsPage.java
@@ -8,6 +8,12 @@ import com.utility.BrowserUtility;
 public class ContactUsPage extends BrowserUtility {
 
 	private static final By SUBJECT_HEADING_LOCATOR = By.xpath("//select[@id=\"id_contact\"]");
+	private static final By ORDER_REFERENCE_LOCATOR = By.xpath("//select[@name=\"id_order\"]/option[2]");
+	private static final By PRODUCT_NAME_LOCATOR = By
+			.xpath("//div[@class=\"selector\"]/select[@name=\"id_product\"]/option[2]");
+	private static final By MESSAGE_TEXTAREA_LOCATOR = By.id("message");
+	private static final By SEND_BUTTON_LOCATOR = By.id("submitMessage");
+	private static final By SUCCESS_MESSAGE_LOCATOR = By.xpath("//div[@id=\"center_column\"]/p");
 	private static final String CUSTOMER_SERVICE_HEADING = "Customer service";
 
 	public ContactUsPage(WebDriver Driver) {
@@ -20,4 +26,29 @@ public class ContactUsPage extends BrowserUtility {
 		return new ContactUsPage(getDriver());
 	}
 
+	public ContactUsPage selectOrderReference() {
+		clickOn(ORDER_REFERENCE_LOCATOR);
+		return new ContactUsPage(getDriver());
+	}
+
+	public ContactUsPage selectProductName() {
+		clickOn(PRODUCT_NAME_LOCATOR);
+		return new ContactUsPage(getDriver());
+	}
+
+	public ContactUsPage enterMessage() {
+		enterText(MESSAGE_TEXTAREA_LOCATOR, "this is the message");
+		return new ContactUsPage(getDriver());
+	}
+
+	public ContactUsPage clickSendButton() {
+		System.out.println("Sending the message");
+		clickOn(SEND_BUTTON_LOCATOR);
+		return new ContactUsPage(getDriver());
+	}
+
+	public String getSuccessMessage() {
+		System.out.println("Getting Success Message");
+		return getVisibleText(SUCCESS_MESSAGE_LOCATOR);
+	}
 }
--- src/test/java/com/ui/tests/ContactCustomerServiceTest.java
+++ src/test/java/com/ui/tests/ContactCustomerServiceTest.java
@@ -1,5 +1,6 @@
 package com.ui.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,6 +17,9 @@ public class ContactCustomerServiceTest extends TestBase {
 
 	@Test(description = "Veryfies if the user can send a message to customer support", groups = { "e2e", "sanity" })
 	public void sendEmailToCustomerSupport() {
-		myAccountPage.goToContactUsPage().selectSubjectHeading();
+		String successMessage = myAccountPage.goToContactUsPage().selectSubjectHeading().selectOrderReference()
+				.selectProductName().enterMessage().clickSendButton().getSuccessMessage();
+
+		Assert.assertEquals(successMessage, "Your message has been successfully sent to our team.");
 	}
 }
